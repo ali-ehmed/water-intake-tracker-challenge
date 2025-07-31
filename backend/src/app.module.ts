987575c53aa98d67df94c74b375875130e56d1e4
shahroz1 <shahroz@dastgyr.com>
2025-08-01 00:14:14 +0500

    Create Water Module and register app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { WaterModule } from './water/water.module';
 
 @Module({
-  imports: [],
+  imports: [WaterModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
